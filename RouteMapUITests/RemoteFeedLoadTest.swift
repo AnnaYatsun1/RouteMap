3896c840b4c395b615f8f114e201b390e0e2ee98
Anna Yatsun <g.yatsun@temabit.com>
2022-08-31 15:25:25 +0300

    remove duplicate code

--- RouteMapUITests/RemoteFeedLoadTest.swift
+++ RouteMapUITests/RemoteFeedLoadTest.swift
@@ -24,29 +24,31 @@ protocol HTTPClient {
     func get(url: URL)
 }
 
-class HTTPClientSpy: HTTPClient {
-    var requestURL: URL?
-    
-    func get(url: URL) {
-        requestURL = url
-    }
-}
-
-
 class RemoteFeedLoadTest: XCTestCase {
 
     func test_init_dosNotRequestDataFromUrl() {
-        let url = URL(string: "http://a-given-url.com")!
-        let client = HTTPClientSpy()
-        _ = RemoteFeedLoad(url: url, client: client)
+        let (_, client) = mekeSUT()
         XCTAssertNil(client.requestURL)
     }
 
     func test_load_requestDataFromUrl() {
-        let client = HTTPClientSpy()
         let url = URL(string: "http://given-url.com")!
-        let sut = RemoteFeedLoad(url: url, client: client)
+        let (sut, client) = mekeSUT(url: url)
         sut.load()
-        XCTAssertNotNil(client.requestURL)
+        XCTAssertEqual(client.requestURL, url)
+    }
+    
+    private func mekeSUT(url: URL = URL(string: "http://a-given-url.com")!) -> (sut: RemoteFeedLoad, client: HTTPClientSpy) {
+        let client = HTTPClientSpy()
+        let sut = RemoteFeedLoad(url: url, client: client)
+        return (sut, client)
+    }
+    
+    private class HTTPClientSpy: HTTPClient {
+        var requestURL: URL?
+        
+        func get(url: URL) {
+            requestURL = url
+        }
     }
 }
