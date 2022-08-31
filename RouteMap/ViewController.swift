a6353a298b45d6e4186da9956d5a29959c5a8c73
Anna Yatsun <g.yatsun@temabit.com>
2022-08-31 12:47:27 +0300

    initial commit

---
+++ RouteMap/ViewController.swift
@@ -0,0 +1,62 @@
+//
+//  ViewController.swift
+//  RouteMap
+//
+//  Created by Анна Яцун on 01.08.2022.
+//
+
+import UIKit
+
+// LogginModule
+
+class ViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        APIClient.instance.login() { user in
+            
+        }
+    }
+}
+
+struct LoggedInUser {}
+
+extension APIClient {
+    func login(completion: (LoggedInUser) -> ()) {
+        
+    }
+}
+
+// FeedModule
+class FeedController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        APIClient.instance.loadFeed() { feedItem in
+            
+        }
+    }
+}
+struct FeedItem {
+    let id: UUID
+    let description: String?
+    let location: String?
+    let image: String
+}
+
+extension APIClient {
+    func loadFeed(completion: ([FeedItem]) -> ()) {
+        
+    }
+}
+
+
+// Singeltone
+class APIClient {
+    
+    static let instance = APIClient()
+    
+    private init() { }
+    
+    func execute(_ : URLSession, completion: (Data) -> ()) {
+        
+    }
+}
